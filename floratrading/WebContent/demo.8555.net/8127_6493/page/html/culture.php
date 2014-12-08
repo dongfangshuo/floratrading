a7b12ccc3b7ca728ffe25e195b426d64cd784cb7
dongfangshuo <gotoworld00@gmail.com>
2014-12-08 08:33:02 +0800

    add template

---
+++ floratrading/WebContent/demo.8555.net/8127_6493/page/html/culture.php
@@ -0,0 +1,401 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=7" />
+<title>Corporate Culture-COMPANY HOMEPAGE</title>
+<meta content="" name="keywords" />
+<meta content="" name="description" />
+<link href="../../base/templates/css/common.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="../../base/js/base.js"></script>
+<script type="text/javascript" src="../../base/js/common.js"></script>
+<script type="text/javascript" src="../../base/js/form.js"></script>
+<script type="text/javascript" src="../../base/js/blockui.js"></script>
+<!-reload-!>
+</head>
+<body style='background:#dddddd url(../../effect/source/bg/bg.png) repeat-x scroll 0% 0%'>
+<script>
+var PDV_PAGEID='472'; 
+var PDV_RP='../../'; 
+var PDV_COLTYPE='page'; 
+var PDV_PAGENAME='html_culture'; 
+</script>
+
+<div id='contain' style='width:1002px;background:none transparent scroll repeat 0% 0%;margin:0px auto;padding:0px'>
+
+<div id='top' style='width:1002px;height:580px;background:none transparent scroll repeat 0% 0%'>
+
+
+<!-- 网站标志 -->
+
+<div id='pdv_16971' class='pdv_class'  title='' style='width:312px;height:87px;top:48px;left:24px; z-index:3'>
+<div id='spdv_16971' class='pdv_top' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:12px;line-height:25px;font-weight:bold;color:">
+		
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="http://demo.8555.net/8127_6493/page/html/-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+
+
+<a href="culture.php#"><img src="../../advs/pics/20121216/1355643068.png"border="0" /></a>
+
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 图片轮播(S1) -->
+
+<div id='pdv_17787' class='pdv_class'  title='' style='width:1002px;height:344px;top:234px;left:0px; z-index:6'>
+<div id='spdv_17787' class='pdv_top' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:12px;line-height:25px;font-weight:bold;color:">
+		
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="http://demo.8555.net/8127_6493/page/html/-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+
+	<link rel="stylesheet" href="http://api.8555.net/css.php?YWFhYWF8ODEyN3xhZHZzc2xpZGVzfDE0MTc5NTc0MDV8OGE2MDFjMjBlYWVkZDUyZTc4OThiY2IxNDllYTVlYWN8" />
+	<script type="text/javascript" src="http://api.8555.net/script.php?YWFhYWF8ODEyN3xhZHZzc2xpZGVzfDE0MTc5NTc0MDV8OGE2MDFjMjBlYWVkZDUyZTc4OThiY2IxNDllYTVlYWN8"></script>
+	<script>
+		$(function(){
+			$('#s1_slides').slides({
+				preload: true,
+				preloadImage: '../../advs/templates/images/s1-loading.gif',
+				play: 5000,
+				pause: 2500,
+				hoverPause: true
+			});
+		});
+	</script>
+
+	<div id="s1_container">
+		<div id="s1_example">
+			
+			<div id="s1_slides">
+				<div class="slides_container">
+
+					<a href='http://' title='' target='_blank'><img src='../../advs/pics/20120301/1330577342.jpg' width='1002' height='330' alt='' border='0'></a>
+
+					<a href='http://' title='' target='_blank'><img src='../../advs/pics/20120301/1330577349.jpg' width='1002' height='330' alt='' border='0'></a>
+
+					<a href='http://' title='' target='_blank'><img src='../../advs/pics/20121117/1353136076.jpg' width='1002' height='330' alt='' border='0'></a>
+
+                </div>
+				
+			</div>
+			
+		</div>
+		<div class="pagination_bg"></div>
+	</div>
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 全站搜索表单 -->
+
+<div id='pdv_17908' class='pdv_class'  title='全站搜索' style='width:443px;height:43px;top:63px;left:540px; z-index:7'>
+<div id='spdv_17908' class='pdv_top' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:12px;line-height:25px;font-weight:bold;color:">
+		全站搜索
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="http://demo.8555.net/8127_6493/page/html/-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+<link href="../../search/templates/css/searchform.css" rel="stylesheet" type="text/css" />
+<script language="javascript" src="../../search/js/searchform.js"></script>
+
+<div class="globalsearchformzone">
+<form id="globalsearchform" method="get" action="http://demo.8555.net/8127_6493/search/index.php">
+   	<div class="globalsearchform">
+    	<input name="key" type="text" id="globalsearchform_key" value="" class="input" />
+  	</div>
+	<div class="globalsearchform1">
+  		<input name="imageField" id="button" type="image" src="../../search/templates/images/searchr.png" class="imgbutton" />
+  	</div>
+</form>
+</div>
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 二级下拉菜单 -->
+
+<div id='pdv_17326' class='pdv_class'   style='width:987px;height:64px;top:169px;left:15px; z-index:8'>
+<div id='spdv_17326' class='pdv_top' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:12px;line-height:25px;font-weight:bold;color:">
+		导航菜单
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="http://demo.8555.net/8127_6493/page/html/-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+
+<link href="../../menu/templates/css/dropmenu.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="http://api.8555.net/script.php?YWFhYWF8ODEyN3xkcm9wbWVudXwxNDE3OTU3NDA1fDhhNjAxYzIwZWFlZGQ1MmU3ODk4YmNiMTQ5ZWE1ZWFjfA=="></script>
+<ul id="dropmenu">
+
+<li><a href="http://demo.8555.net/8127_6493/index.php">HOME</a>
+
+</li>
+
+<li><a href="company.php">ABOUT US</a>
+<ul>
+
+<li><a href="company.php">About Us</a></li>
+
+<li><a href="culture.php">Culture</a></li>
+
+<li><a href="equipment.php">Equipment</a></li>
+
+<li><a href="cert.php">Certificate</a></li>
+
+<li><a href="service.php">Service</a></li>
+</ul>
+
+</li>
+
+<li><a href="http://demo.8555.net/8127_6493/product/class/">PRODUCTS</a>
+
+</li>
+
+<li><a href="http://demo.8555.net/8127_6493/news/class/">NEWS</a>
+<ul>
+
+<li><a href="http://demo.8555.net/8127_6493/news/class/?86.html">News</a></li>
+
+<li><a href="http://demo.8555.net/8127_6493/news/class/?113.html">Infomation</a></li>
+</ul>
+
+</li>
+
+<li><a href="equipment.php">EQUIPMENT</a>
+
+</li>
+
+<li><a href="http://demo.8555.net/8127_6493/job/">JOBS</a>
+
+</li>
+
+<li><a href="http://demo.8555.net/8127_6493/page/contact/contact.php">CONTACT US</a>
+<ul>
+
+<li><a href="http://demo.8555.net/8127_6493/page/contact/contact.php">Contact Us</a></li>
+
+<li><a href="http://demo.8555.net/8127_6493/page/contact/feedback.php">Feedback</a></li>
+</ul>
+
+</li>
+ 
+</ul>
+<div class="clear"> </div>
+
+</div>
+</div>
+
+</div>
+</div>
+</div>
+<div id='content' style='width:1002px;height:400px;background:url(../../effect/source/bg/cbg.png);margin:0px auto'>
+
+
+<!-- 当前位置提示条 -->
+
+<div id='pdv_16385' class='pdv_class'  title='当前位置' style='width:752px;height:41px;top:0px;left:250px; z-index:2'>
+<div id='spdv_16385' class='pdv_content' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="border:0px;height:100%;padding:0;margin:0;background:url(../../base/border/788/images/title.png) no-repeat">
+	<div style="float:right;width:11px;height:100%;background:url(../../base/border/788/images/title.png) -989px 0px no-repeat;"></div>
+ 
+<link href="../templates/css/nav.css" rel="stylesheet" type="text/css" />
+<div id="nav">
+<a href="http://demo.8555.net/8127_6493/">Home</a> &gt; Corporate Culture
+
+</div>
+
+</div>
+
+</div>
+</div>
+
+<!-- 网页标题(菜单) -->
+
+<div id='pdv_17534' class='pdv_class'  title='Menu' style='width:250px;height:231px;top:0px;left:0px; z-index:4'>
+<div id='spdv_17534' class='pdv_content' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="border:0px;height:100%;padding:0;margin:0;">
+<div style="height:41px;border:0px;padding:0;margin:0;background:url(../../base/border/776/images/title.png) repeat-x">
+<div style="float:left;font:14px/41px Verdana,microsoft yahei,Arial;padding-left:20px;color:#fff;">
+	Menu
+	</div>
+		<div style="float:right;width:60px;height:41px;margin-right:5px"><a href="http://demo.8555.net/8127_6493/page/html/-1" style="display:none;font:10px/41px Verdana,microsoft yahei,Arial;color:#fff;">MORE &gt;&gt;</a></div>
+</div>
+
+<div style="margin:0px;padding:0px;">
+
+<link href="http://api.8555.net/css.php?YWFhYWF8ODEyN3xjbGFzc19lbnwxNDE3OTU3NDA1fDhhNjAxYzIwZWFlZGQ1MmU3ODk4YmNiMTQ5ZWE1ZWFjfA==" rel="stylesheet" type="text/css" />
+<div class="class_en">
+
+
+<a href="company.php" target="_self" class="class_en">About Us</a>
+
+
+<a href="culture.php" target="_self" class="class_en">Corporate Culture</a>
+
+
+<a href="equipment.php" target="_self" class="class_en">Equipment</a>
+
+
+<a href="cert.php" target="_self" class="class_en">Certificate</a>
+
+
+<a href="service.php" target="_self" class="class_en">Service</a>
+ 
+</div>
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 网页内容详情 -->
+
+<div id='pdv_16387' class='pdv_class'  title='' style='width:705px;height:327px;top:71px;left:278px; z-index:5'>
+<div id='spdv_16387' class='pdv_content' style='overflow:visible;width:100%;'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:12px;line-height:25px;font-weight:bold;color:">
+		
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="http://demo.8555.net/8127_6493/page/html/-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+<link href="../templates/css/pagecontent.css" rel="stylesheet" type="text/css" />
+<div id='pagecontent' class='page_content'><p>HangZhou Some Electronics Co., Ltd. specializes in R&amp;D, production and sales of precision connectors. Its products are used in computers, telecom network equipments, industrial apparatuses, ect. The Company is armed with advanced precision mold manufacturing equipment, high-speed hardware pressing equipment, precision plastic injection equipment, automatic assembly line and inspection/testing devices.The company adheres to the business principle of "technology for development and quality for existence", and provides customers with quality products and considerate service based on its years of experience in R&amp;D, procuction and management, great technical strength and the mature quality management system.</p>
+<p>HangZhou Some Electronics Co., Ltd. specializes in R&amp;D, production and sales of precision connectors. Its products are used in computers, telecom network equipments, industrial apparatuses, ect. The Company is armed with advanced precision mold manufacturing equipment, high-speed hardware pressing equipment, precision plastic injection equipment, automatic assembly line and inspection/testing devices.The company adheres to the business principle of "technology for development and quality for existence", and provides customers with quality products and considerate service based on its years of experience in R&amp;D, procuction and management, great technical strength and the mature quality management system.</p></div>
+<script>
+
+$(window).load(function(){
+	$("#pagecontent").find("img").hide();
+	var w=$("#pagecontent")[0].offsetWidth;
+	$("#pagecontent").find("img").each(function(){
+		$(this).show();
+		if(this.offsetWidth>w){
+			this.style.width=w + "px";
+		}
+	});
+		
+});
+
+</script>
+
+</div>
+</div>
+
+</div>
+</div>
+</div>
+<div id='bottom' style='width:1002px;height:147px;background:url(../../effect/source/bg/bbg.png) repeat-x'>
+
+
+<!-- 底部信息编辑区 -->
+
+<div id='pdv_16978' class='pdv_class'  title='脚注信息' style='width:380px;height:99px;top:53px;left:316px; z-index:1'>
+<div id='spdv_16978' class='pdv_bottom' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:12px;line-height:25px;font-weight:bold;color:">
+		脚注信息
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="http://demo.8555.net/8127_6493/page/html/-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+<div style="width:100%;text-align:center;font:12px/20px Arial, Helvetica, sans-serif">
+<div style="TEXT-ALIGN: center; LINE-HEIGHT: 24px; MARGIN: 0px; FONT-FAMILY: Verdana, Arial, Helvetica, sans-serif; COLOR: #fff">Copyright(C)2009-2012 Building Materials company </div>
+</div>
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 底部导航菜单 -->
+
+<div id='pdv_17428' class='pdv_class'   style='width:900px;height:28px;top:23px;left:49px; z-index:9'>
+<div id='spdv_17428' class='pdv_bottom' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:12px;line-height:25px;font-weight:bold;color:">
+		脚注栏目
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="http://demo.8555.net/8127_6493/page/html/-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+
+<link href="../../menu/templates/css/bottommenu.css" rel="stylesheet" type="text/css" />
+<div id="bottommenu">|
+
+<a href="company.php" target="_self">About us</a>|
+
+<a href="http://demo.8555.net/8127_6493/job/index.php" target="_self">Jobs</a>|
+
+<a href="http://demo.8555.net/8127_6493/down/class" target="_self">Download</a>|
+
+<a href="http://demo.8555.net/8127_6493/page/contact/contact.php" target="_self">Contact Us</a>|
+
+<a href="http://demo.8555.net/8127_6493/page/contact/feedback.php" target="_self">Feedback</a>|
+
+<a href="http://demo.8555.net/8127_6493/advs/link/" target="_self">Links</a>|
+ 
+</div>
+
+
+</div>
+</div>
+
+</div>
+</div>
+</div>
+</div><div id='bodyex'>
+
+</div>
+<div id='topex' style='display:none;width:100%;height:580px;background:none transparent scroll repeat 0% 0%'>
+</div>
+<div id='contentex' style='display:none;width:100%;height:400px;background:url(../../effect/source/bg/cbg.png)'>
+</div>
+<div id='bottomex' style='display:none;width:100%;height:147px;background:url(../../effect/source/bg/bbg.png) repeat-x'>
+</div>
+<div id='advsex'></div>
+
+</body>
+</html>
